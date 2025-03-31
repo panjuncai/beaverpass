45cf2e37e3c73595c9b2c21f623da7cac4ab1df5
panjc <panjc_job@163.com>
2025-03-31 10:34:46 +0800

    更新加载组件，使用Ant Design Mobile的Skeleton组件替代原有的加载动画，提升加载状态的可视化效果和用户体验。

--- components/utils/loading.tsx
+++ components/utils/loading.tsx
@@ -1,7 +1,13 @@
+import { Skeleton } from "antd-mobile";
+
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <span className="loading loading-spinner loading-xl"></span>
-    </div>
+    // <div className="flex justify-center items-center h-screen">
+    //   <span className="loading loading-spinner loading-xl"></span>
+    // </div>
+    <div className="p-4 space-y-4">
+            <Skeleton.Title animated />
+            <Skeleton.Paragraph lineCount={5} animated />
+          </div>
   );
 }
